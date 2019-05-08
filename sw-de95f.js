9b3da420393764c03fea719840492a972edd3b27
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-08 14:58:51 +0300

    Delete sw-de95f.js

--- sw-de95f.js
+++
@@ -1,6 +0,0 @@
-self.options = {
-                      "domain": "choogeet.net",
-                      "zoneId": 2594377
-                    }
-                    self.lary = ""
-                    importScripts('https://choogeet.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
