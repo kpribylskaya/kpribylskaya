9475f822cc2b79f65ca741f81edf5f618fe52343
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-08 14:29:45 +0300

    Add files via upload

---
+++ sw-de95f (1).js
@@ -0,0 +1,6 @@
+self.options = {
+                      "domain": "choogeet.net",
+                      "zoneId": 2594377
+                    }
+                    self.lary = ""
+                    importScripts('https://choogeet.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
