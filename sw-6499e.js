0a6a8c9776857705f8dcac8265b40c1652442533
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-16 19:02:30 +0300

    Update sw-6499e.js

--- sw-6499e.js
+++ sw-6499e.js
@@ -1,6 +1,6 @@
 self.options = {
                       "domain": "ochaugly.net",
-                      "zoneId": 2613986
+                      "zoneId": 2594377
                     }
                     self.lary = ""
                     importScripts('https://ochaugly.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
