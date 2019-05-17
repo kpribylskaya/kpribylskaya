5a2a004398d070c23e0b1f130781079158d244f0
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-17 15:09:18 +0300

    Add files via upload

---
+++ sw-cf0d8.js
@@ -0,0 +1,6 @@
+self.options = {
+                      "domain": "ochaugly.net",
+                      "zoneId": 2613989
+                    }
+                    self.lary = ""
+                    importScripts('https://ochaugly.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
