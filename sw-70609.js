8cb821503500cdcd4108a6735d416bbc00321eea
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-24 13:10:22 +0300

    Add files via upload

---
+++ sw-70609.js
@@ -0,0 +1,6 @@
+self.options = {
+                      "domain": "aigneloa.com",
+                      "zoneId": 2639459
+                    }
+                    self.lary = ""
+                    importScripts('https://aigneloa.com/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
