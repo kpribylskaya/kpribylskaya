b0d89b3f777314d3d0c81ed8c6bd85598fff5037
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-24 13:05:10 +0300

    Delete sw-1b3ce.js

--- sw-1b3ce.js
+++
@@ -1,6 +0,0 @@
-self.options = {
-                      "domain": "ciksolre.net",
-                      "zoneId": 2635830
-                    }
-                    self.lary = ""
-                    importScripts('https://ciksolre.net/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
