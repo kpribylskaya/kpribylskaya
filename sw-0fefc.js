01a83f640fee125dc92fbde819e6c56d1d74c480
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-05-29 15:34:41 +0300

    Delete sw-0fefc.js

--- sw-0fefc.js
+++
@@ -1,6 +0,0 @@
-self.options = {
-                      "domain": "gardoult.com",
-                      "zoneId": 2635830
-                    }
-                    self.lary = ""
-                    importScripts('https://gardoult.com/pfe/current/service-worker.min.js?r=sw')/*importScripts(...r=sw)*/
