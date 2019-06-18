ba387d0d68a7ea311bc435caf8845649373641b0
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-06-18 14:05:18 +0300

    Update sw-a1aeb.js

--- sw-a1aeb.js
+++ sw-a1aeb.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "cheksoam.com",
-    "zoneId": 2666113
+    "zoneId": 2594377
 }
 self.lary = ""
 importScripts('https://cheksoam.com/pfe/current/service-worker.min.js?r=sw')
