0a78f3609c01955d2e12b991fccb4a60d3f4b795
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-07-18 14:07:30 +0300

    Update sw-2878d.js

--- sw-2878d.js
+++ sw-2878d.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "jootizud.net",
-    "zoneId": 2683441
+    "zoneId": 2594377
 }
 self.lary = ""
 importScripts('https://jootizud.net/pfe/current/service-worker.min.js?r=sw')
