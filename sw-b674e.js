bca04b83047d411e642be1041b05cca06e06512f
Pribylskaya Ksenia <k.pribylskaya@propellerads.net>
2019-07-18 15:14:54 +0300

    Update sw-b674e.js

--- sw-b674e.js
+++ sw-b674e.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "ciksolre.net",
-    "zoneId": 2671338
+    "zoneId": 2594377
 }
 self.lary = ""
 importScripts('https://ciksolre.net/pfe/current/service-worker.min.js?r=sw')
